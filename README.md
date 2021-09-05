4d112005c22131f0445228c539e7d6a566613464
Brandon Tan <brandontan99@ymail.com>
2021-09-05 22:10:28 +0800

    added some azureml sdk functions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # bond-price-forcasting
 ## Setup the environment
-1. python -m pip venv venv/ 
-2. venv/Scripts/activate 
-3. pip install --upgrade pip
-4. pip install -r requirements.txt (streamlit & azureml sdk)
+1. ensure python version 3.7
+2. python -m virtualenv  venv/ 
+3. venv/Scripts/activate 
+4. pip install --upgrade pip --user
+5. pip install -r requirements.txt (streamlit & azureml sdk)
--- streamlit/azureml_sdk_utils/azureml_sdk_utils.py
+++ streamlit/azureml_sdk_utils/azureml_sdk_utils.py
@@ -2,6 +2,7 @@ import azureml.core
 import pandas as pd
 import logging
 import os
+import numpy as np
 
 from azureml.core.workspace import Workspace
 from azureml.core.experiment import Experiment
@@ -12,12 +13,17 @@ from azureml.core.dataset import Dataset
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core.compute import ComputeTarget, AmlCompute
 from azureml.widgets import RunDetails
+from azureml.core.run import Run
+from azureml.core.model import Model
+from azureml.train.automl.run import AutoMLRun
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 # get auth code
 SVC_PR = ServicePrincipalAuthentication(
@@ -95,10 +101,60 @@ def auto_feature_selection(dataset_df, method, k, target_column_name, show_disca
 
     return dataset_df.iloc[:,cols+[target_column_name]]
 
+# show a list of experiments
+def show_all_experiments():
+    return Experiment.list(WS)
+
+# select an experiment
+def select_experiment(experiment_name):
+    return Experiment(WS, experiment_name)
+
+# show a list of runs from an experiment
+def show_all_runs(experiment):
+    return experiment.get_runs(include_children=True)
+
+# select the best run in an experiment
+def select_best_run(experiment):
+    return experiment.get_runs(include_children=False)
+
+# show a list of registered models
+def show_all_models():
+    return Model.list(WS)
+
+# select a registered model
+def select_registered_model(model_name):
+    return Model(WS, model_name)
+
+# select the best model from an experiment
+def select_best_model(experiment, run_id):
+    automl_run = AutoMLRun(experiment, run_id = run_id)
+    best_run, fitted_model = automl_run.get_output()
+    print(fitted_model.steps)
+    return fitted_model
+
+# make an inference with a model
+def make_inference(model, x_future, y_future):
+    model.quantiles = [0.05,0.5, 0.9] # provides the confidence interval
+    result = model.forecast_quantiles(x_future, y_future)
+    return result
+
+# evaluate test set with root mean squared error
+def evaluate_test_set(model, test_dataset, target_column):
+    test_dataset["VALUE DATE MONTH"] = test_dataset["VALUE DATE MONTH"].astype('datetime64[ns]')
+    x_test = test_dataset.copy()
+    y_test = x_test.pop(target_column).values
+    label_query = y_test.copy().astype(np.float)
+    label_query.fill(np.nan)
+    y_pred_df = model.forecast(x_test, label_query)[1]
+    y_pred_df.dropna(subset=["_automl_target_col"],inplace=True) # _automl_target_col is the y_pred
+    result = y_pred_df.merge(test_dataset[["VALUE DATE MONTH","STOCK CODE","NEXT MONTH CHANGES IN EVAL MID PRICE"]], on=["VALUE DATE MONTH","STOCK CODE"])
+    rmse = sqrt(mean_squared_error(result[target_column], result["_automl_target_col"]))
+    return result, rmse
+
 # train a model on the cloud only 
 def train_model(dataset_df, experiment_name, time_column_name, time_series_id_column_names, target_column_name, experiment_timeout_hours=24):
     # create or retrieve experiment
-    experiment = Experiment(WS, experiment_name)
+    experiment = select_experiment(experiment_name)
     forecasting_parameters = ForecastingParameters(
         time_column_name=time_column_name,
         forecast_horizon=1, # forecast 1 month ahead only
@@ -127,9 +183,3 @@ def train_model(dataset_df, experiment_name, time_column_name, time_series_id_co
     # https://github.com/Azure/MachineLearningNotebooks/blob/master/how-to-use-azureml/automated-machine-learning/local-run-classification-credit-card-fraud/auto-ml-classification-credit-card-fraud-local.ipynb
     remote_run.wait_for_completion()
     return remote_run.get_output()
-
-
-
-# upload_dataset("dataset_31082021.xlsx")
-# print(show_all_registered_datasets())
-# best_run, fitted_model = train_model("dataset", "test", "VALUE DATE MONTH", "STOCK CODE", "NEXT MONTH CHANGES IN EVAL MID PRICE")
