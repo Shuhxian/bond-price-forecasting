4d1a131283d8b27c171b195a0a4326b99fdfd762
Goy Shuh Xian <50050582+Shuhxian@users.noreply.github.com>
2021-09-08 05:45:41 +0800

    Update model_page.py

--- streamlit/pages/model_page.py
+++ streamlit/pages/model_page.py
@@ -10,7 +10,7 @@ from azureml.core.workspace import Workspace
 from azureml.core.experiment import Experiment
 from azureml.train.automl.run import AutoMLRun
 
-def fetch_newest(fetch=False,csv='exp.csv'):
+def fetch_newest(fetch=False,csv='/streamlit/pages/exp.csv'):
   if fetch:
     
     # get auth code
@@ -20,7 +20,7 @@ def fetch_newest(fetch=False,csv='exp.csv'):
     service_principal_password="eE.ozUaf8ncZt8~5gID-A7X85g.7clQ.P6")
 
     # define workspace
-    WS = Workspace.from_config("config.json", auth=SVC_PR)
+    WS = Workspace.from_config("/azureml-sdk-utils/config.json", auth=SVC_PR)
 
     experiments=[]
     run_ids=[]
